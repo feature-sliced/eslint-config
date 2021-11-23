93332a20e252df46c35bc9911bb1963c6e9b3c80
Evgeniy <daem0nbfg@gmail.com>
2021-11-23 21:18:50 +0300

    feat: import boundaries (between layers).

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
+const FS_LAYERS = [
+    "app",
+    "processes",
+    "widgets",
+    "pages",
+    "features",
+    "entities",
+    "shared",
+];
+
+const lowerThenLayer = (type, layers) => layers.slice(layers.indexOf(type) + 1)
+
 module.exports = {
+    parser: "@typescript-eslint/parser",
     parserOptions: {
-        ecmaVersion: "2015",
+        ecmaVersion: 2015,
+        project: "tsconfig.json",
         sourceType: "module",
     },
-    plugins: [
-        "import",
-    ],
+    plugins: ["boundaries"],
+    extends: ["plugin:boundaries/recommended"],
+    ignorePatterns: ['.eslintrc.js'],
+    settings: {
+        "import/resolver": {
+            typescript: {
+                alwaysTryTypes: true,
+            },
+        },
+        "boundaries/elements":
+            FS_LAYERS.map((layer) => ({
+                type: layer,
+                pattern: `${layer}`,
+            }))
+    },
     rules: {
+        "boundaries/element-types": [
+            2,
+            {
+                "default": "disallow",
+                "message": "${file.type} is not allowed to import ${dependency.type}",
+                "rules": FS_LAYERS.map((layer) => ({
+                    from: layer,
+                    allow: lowerThenLayer(layer, FS_LAYERS)
+                }))
+            }
+        ],
     },
 };
