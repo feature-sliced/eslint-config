e2355626e1cb01d3b27ab0d8cb5df4e679d11b12
Ilya Azin <martis.azin@gmail.com>
2021-11-23 10:55:10 +0300

    refactor: split legacy and cleaned rules
    #12

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// Allowed paths for public API
-const PUBLIC_PATHS = [
-    "app",
-    "pages",
-    "features",
-    "shared",
-    "shared/**",
-    "models",
-];
-// Private imports are prohibited, use public imports instead
-const PRIVATE_PATHS = [
-    "app/**",
-    "pages/**",
-    "features/**",
-    "shared/*/**",
-];
-// Prefer absolute imports instead of relatives (for root modules)
-const RELATIVE_PATHS = [
-    "../**/app",
-    "../**/pages",
-    "../**/features",
-    "../**/shared",
-    "../**/models",
-];
-
 module.exports = {
     parserOptions: {
         ecmaVersion: "2015",
@@ -32,28 +7,5 @@ module.exports = {
         "import",
     ],
     rules: {
-        "import/first": 2,
-        "import/no-unresolved": 0, // experimental
-        "import/order": [
-            2,
-            {
-                pathGroups: PUBLIC_PATHS.map(
-                    (pattern) => ({
-                        pattern,
-                        group: "internal",
-                        position: "after",
-                    }),
-                ),
-                pathGroupsExcludedImportTypes: ["builtin"],
-                groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
-            },
-        ],
-        // TODO: with messages (https://github.com/feature-sliced/eslint-config/issues/3)
-        "no-restricted-imports": [
-            2, 
-            { 
-                patterns: [...PRIVATE_PATHS, ...RELATIVE_PATHS] 
-            }
-        ],
     },
 };
---
+++ index.legacy.js
@@ -0,0 +1,65 @@
+/**
+ * DO NOT MODIFY THIS FILE
+ * THERE ARE ONLY OLD RULES THAT SHOULD BE ACTUALIZED
+ * @see https://github.com/feature-sliced/eslint-config/issues/17
+ */
+
+// Allowed paths for public API
+const PUBLIC_PATHS = [
+    "app",
+    "pages",
+    "features",
+    "shared",
+    "shared/**",
+    "models",
+];
+// Private imports are prohibited, use public imports instead
+const PRIVATE_PATHS = [
+    "app/**",
+    "pages/**",
+    "features/**",
+    "shared/*/**",
+];
+// Prefer absolute imports instead of relatives (for root modules)
+const RELATIVE_PATHS = [
+    "../**/app",
+    "../**/pages",
+    "../**/features",
+    "../**/shared",
+    "../**/models",
+];
+
+module.exports = {
+    parserOptions: {
+        ecmaVersion: "2015",
+        sourceType: "module",
+    },
+    plugins: [
+        "import",
+    ],
+    rules: {
+        "import/first": 2,
+        "import/no-unresolved": 0, // experimental
+        "import/order": [
+            2,
+            {
+                pathGroups: PUBLIC_PATHS.map(
+                    (pattern) => ({
+                        pattern,
+                        group: "internal",
+                        position: "after",
+                    }),
+                ),
+                pathGroupsExcludedImportTypes: ["builtin"],
+                groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+            },
+        ],
+        // TODO: with messages (https://github.com/feature-sliced/eslint-config/issues/3)
+        "no-restricted-imports": [
+            2, 
+            { 
+                patterns: [...PRIVATE_PATHS, ...RELATIVE_PATHS] 
+            }
+        ],
+    },
+};
