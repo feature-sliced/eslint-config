ae4241e3010d66e9d42cc63538b4590c2c4960ce
Ilya Azin <martis.azin@gmail.com>
2021-11-24 23:49:21 +0300

    fix: typo and added gitignore for webstorm ide config folder.

--- index.js
+++ index.js
@@ -12,37 +12,30 @@ const lowerThenLayer = (type, layers) => layers.slice(layers.indexOf(type) + 1)
 
 module.exports = {
     parserOptions: {
-        ecmaVersion: "2015",
-        sourceType: "module",
+        "ecmaVersion": "2015",
+        "sourceType": "module",
     },
     plugins: ["boundaries", "import"],
     extends: ["plugin:boundaries/recommended"],
-    ignorePatterns: ['.eslintrc.js'],
+    ignorePatterns: [".eslintrc.js"],
     settings: {
-        "import/resolver": {
-            typescript: {
-                alwaysTryTypes: true,
-            },
-        },
         "boundaries/elements":
             FS_LAYERS.map((layer) => ({
-                type: layer,
-                pattern: `${layer}/*`,
-                mode: "folder",
-                capture: ["slices"]
+                "type": layer,
+                "pattern": `${layer}/*`,
+                "mode": "folder",
+                "capture": ["slices"]
             }))
     },
     rules: {
-        // FIXME: debug only
-        "import/first": 2,
         "boundaries/element-types": [
             2,
             {
                 "default": "disallow",
                 "message": "${file.type} is not allowed to import ${dependency.type}",
                 "rules": FS_LAYERS.map((layer) => ({
-                    from: layer,
-                    allow: lowerThenLayer(layer, FS_LAYERS)
+                    "from": layer,
+                    "allow": lowerThenLayer(layer, FS_LAYERS)
                 }))
             }
         ],
