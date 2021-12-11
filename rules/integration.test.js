fa18676a34d3a65524c9b5bf55ea0cbe5e20d413
Evgeniy <daem0nbfg@gmail.com>
2021-12-12 00:36:25 +0300

    fix: removed redundant console.log

--- rules/integration.test.js
+++ rules/integration.test.js
@@ -25,8 +25,6 @@ describe("Integration tests:", () => {
             filePath: "src/widgets/mock/index.js",
         });
 
-        console.log(report[0]);
-
         assert.strictEqual(report[0].errorCount, 11);
     });
 
