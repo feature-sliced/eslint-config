2f6101b688fbb2b96f64d390bc2fb3aea19b8e75
Evgeniy <daem0nbfg@gmail.com>
2021-12-11 23:16:06 +0300

    test: validate global config.

--- test/config.test.js
+++ test/config.test.js
@@ -1,30 +1,48 @@
 const assert = require("assert");
-const cfg = require("../");
 const utils = require("../utils/tools");
+const cfg = require("../");
 
 describe("config is valid", () => {
-    it("parserOptions ~ Record<string, string>", () => {
+    it("Valid parerOptions should presented in global config.", () => {
         assert.ok(utils.isObj(cfg.parserOptions));
         Object.entries(cfg.parserOptions).forEach(([key, value]) => {
             assert.ok(utils.isString(key));
             assert.ok(utils.isString(value));
-        })
-    })
-    // FIXME: Recursive plugin parsing
-    it.skip("plugins ~ Array", () => {
-        assert.ok(utils.isArray(cfg.plugins));
-    })
-    it.skip("rules ~ Record<string, Options>", () => {
-        assert.ok(utils.isObj(cfg.rules));
-        Object.entries(cfg.rules).forEach(([ruleName, ruleOptions]) => {
-            assert.ok(utils.isString(ruleName));
-            assert.ok(
-                utils.isNumber(ruleOptions) ||
-                utils.isArray(ruleOptions) ||
-                utils.isObj(ruleOptions)
-            );
-        })
-    })
+        });
+    });
+
+    it("Global config should extends other config.", () => {
+        assert.ok(utils.isArray(cfg.extends));
+    });
+
+    it("All extended configs should be presented.", () => {
+        cfg.extends.forEach((configPath) => {
+            const config = require(configPath);
+            assert.ok(config);
+        });
+    });
+
+    it("All extended configs plugins should be presented as Array's", () => {
+        cfg.extends.forEach((configPath) => {
+            const config = require(configPath);
+            assert.ok(utils.isArray(config.plugins));
+        });
+    });
+
+    it("All extended configs rules should be with name and value", () => {
+        cfg.extends.forEach((configPath) => {
+            const config = require(configPath);
+            assert.ok(utils.isObj(config.rules));
+            Object.entries(config.rules).forEach(([ruleName, ruleOptions]) => {
+                assert.ok(utils.isString(ruleName));
+                assert.ok(
+                    utils.isNumber(ruleOptions) ||
+                    utils.isArray(ruleOptions) ||
+                    utils.isObj(ruleOptions),
+                );
+            });
+        });
+    });
     // TODO: eslint.isValid
     // NOTE: maybe will need... in future...
     // t.ok(isObject(config.parserOptions))
