4343a9d261a39daa203321922edb7b10ab5392f1
Evgeniy <daem0nbfg@gmail.com>
2021-12-12 01:29:18 +0300

    refactor: jedi test way ;)

--- rules/public-api-boundaries/index.test.js
+++ rules/public-api-boundaries/index.test.js
@@ -10,44 +10,39 @@ const eslint = new ESLint({
 
 describe("PublicAPI import boundaries:", () => {
     it("Should lint PublicAPI boundaries with errors.", async () => {
-        const wrongImports = [
-            `import { Issues } from "pages/issues/ui";`,
-            `import { IssueDetails } from "widgets/issue-details/ui/details"`,
-            `import { AuthForm } from "features/auth-form/ui/form"`,
-            `import { Button } from "shared/ui/button/button";`,
-            `import { saveOrder } from "entities/order/model/actions";`,
-            `import { orderModel } from "entities/order/model";`,
-            `import { TicketCard } from "@app/entities/ticket/ui";`,
-        ];
-
-        const report = await eslint.lintText(wrongImports.join("\n"),
+        const report = await eslint.lintText(`
+        import { Issues } from "pages/issues/ui";
+        import { IssueDetails } from "widgets/issue-details/ui/details";
+        import { AuthForm } from "features/auth-form/ui/form";
+        import { Button } from "shared/ui/button/button";
+        import { saveOrder } from "entities/order/model/actions";
+        import { orderModel } from "entities/order/model";
+        import { TicketCard } from "@app/entities/ticket/ui";
+        `,
             { filePath: "src/app/ui/index.js" });
-        assert.strictEqual(report[0].errorCount, wrongImports.length);
+        assert.strictEqual(report[0].errorCount, 7);
     });
 
     it("Should lint PublicAPI boundaries without errors.", async () => {
-        const validCodeSnippet = [
-            `import { Issues } from "pages/issues";`,
-            `import { GoodIssues } from "@app/pages/issues";`,
-            `import { IssueDetails } from "widgets/issue-details"`,
-            `import { AuthForm } from "features/auth-form"`,
-            `import { Button } from "shared/ui/button";`,
-            `import { orderModel } from "entities/order";`,
-            `import { TicketCard } from "@/entities/ticket";`,
-            `import { FixButton } from "@app/shared/ui/fix-button";`,
-        ].join("\n");
-
-        const report = await eslint.lintText(validCodeSnippet, { filePath: "src/app/ui/index.js" });
+        const report = await eslint.lintText(`
+        import { Issues } from "pages/issues";
+        import { GoodIssues } from "@app/pages/issues";
+        import { IssueDetails } from "widgets/issue-details";
+        import { AuthForm } from "features/auth-form";
+        import { Button } from "shared/ui/button";
+        import { orderModel } from "entities/order";
+        import { TicketCard } from "@/entities/ticket";
+        import { FixButton } from "@app/shared/ui/fix-button";
+        `, { filePath: "src/app/ui/index.js" });
         assert.strictEqual(report[0].errorCount, 0);
     });
 
     it("Should lint extra PublicAPI boundaries cases without errors.", async () => {
-        const validCodeSnippet = [
-            `import { AuthForm } from "features/auth/form"`,
-            `import { Button } from "shared/ui";`,
-        ].join("\n");
+        const report = await eslint.lintText(`
+        import { AuthForm } from "features/auth/form";
+        import { Button } from "shared/ui";
+        `, { filePath: "src/app/ui/index.js" });
 
-        const report = await eslint.lintText(validCodeSnippet, { filePath: "src/app/ui/index.js" });
         assert.strictEqual(report[0].errorCount, 0);
     });
 
