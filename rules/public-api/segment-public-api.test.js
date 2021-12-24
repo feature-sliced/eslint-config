555b4f3dd55d74253bd1157fd73bf2b65190a98e
Krakazybik <daem0nbfg@gmail.com>
2021-12-25 00:10:48 +0300

    refactor: Exclusive Segments Public API tests for shared layer.

--- rules/public-api/segment-public-api.test.js
+++ rules/public-api/segment-public-api.test.js
@@ -13,29 +13,45 @@ const eslint = new ESLint({
 describe("Segment PublicAPI:", () => {
     it("Should lint Segment PublicAPI boundaries with errors.", async () => {
         const report = await eslint.lintText(`
-        import { Button } from "shared/ui/button/button";
-        import { Date } from "shared/lib/date/date";
         export { SubmitButton } from "./ui/button";
         export { SmthForm } from "./ui/form";
         export * from "./model/actions";
         export { selectSmthById } from "./model/selectors";
         `,
             { filePath: "src/features/smth/index.ts" });
-        assert.strictEqual(report[0].errorCount, 6);
+        assert.strictEqual(report[0].errorCount, 4);
     });
 
     it("Should lint Segment PublicAPI boundaries without errors.", async () => {
         const report = await eslint.lintText(`
-        import { Button } from "shared/ui/button";
-        import { SexyButton } from "shared/ui";
-        import { Parser } from "shared/lib";
-        import { Date } from "shared/lib/date";  
         export { SubmitButton, SmthForm } from "./ui";
         export * from "./model";
         export { selectSmthById, selectSmthByName } from "./model";
         `, { filePath: "src/features/smth/index.ts" });
 
         assert.strictEqual(report[0].errorCount, 0);
+    });
+
+    describe("Exclusive Segments Public API for shared layer:", () => {
+        it("Should lint with errors.", async () => {
+            const report = await eslint.lintText(`
+            import { Button } from "shared/ui/button/button";
+            import { Date } from "shared/lib/date/date";
+            `,
+                { filePath: "src/features/smth/index.ts" });
+            assert.strictEqual(report[0].errorCount, 2);
+        });
+
+        it("Should lint without errors.", async () => {
+            const report = await eslint.lintText(`
+            import { Button } from "shared/ui/button";
+            import { SexyButton } from "shared/ui";
+            import { Parser } from "shared/lib";
+            import { Date } from "shared/lib/date";  
+            `, { filePath: "src/features/smth/index.ts" });
+
+            assert.strictEqual(report[0].errorCount, 0);
+        });
     });
 
 });
