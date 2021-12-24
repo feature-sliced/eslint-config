b784ea1d6d392e6be6d6d6a3a0293b65b043be52
Krakazybik <daem0nbfg@gmail.com>
2021-12-24 22:57:16 +0300

    test: additional test cases

--- rules/public-api/segment-public-api.test.js
+++ rules/public-api/segment-public-api.test.js
@@ -13,20 +13,26 @@ const eslint = new ESLint({
 describe("Segment PublicAPI:", () => {
     it("Should lint Segment PublicAPI boundaries with errors.", async () => {
         const report = await eslint.lintText(`
+        import { Button } from "shared/ui/button/button";
+        import { Date } from "shared/lib/date/date";
         export { SubmitButton } from "./ui/button";
         export { SmthForm } from "./ui/form";
         export * from "./model/actions";
         export { selectSmthById } from "./model/selectors";
         `,
             { filePath: "src/features/smth/index.ts" });
-        assert.strictEqual(report[0].errorCount, 4);
+        assert.strictEqual(report[0].errorCount, 6);
     });
 
     it("Should lint Segment PublicAPI boundaries without errors.", async () => {
         const report = await eslint.lintText(`
+        import { Button } from "shared/ui/button";
+        import { SexyButton } from "shared/ui";
+        import { Parser } from "shared/lib";
+        import { Date } from "shared/lib/date";  
         export { SubmitButton, SmthForm } from "./ui";
         export * from "./model";
-        export { selectSmthById, selectSmthByName } from "./model";        
+        export { selectSmthById, selectSmthByName } from "./model";
         `, { filePath: "src/features/smth/index.ts" });
 
         assert.strictEqual(report[0].errorCount, 0);
